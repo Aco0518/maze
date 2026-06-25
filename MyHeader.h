650d5bf5d8eec41350456ca527586f84b05f0835
Aco0518 <acooo0518@gmail.com>
2026-06-25 13:10:25 +0900

    Add files via upload

--- MyHeader.h
+++ MyHeader.h
@@ -1,16 +1,18 @@
 #pragma once
+
+void Print_Main();
 void Choose_Difficulty();
 void Print_LeaderBoard();
-void Print_Main();
-void gotoxy(int, int);
-void read_maze();
+void settings();
+void exit_game();
+int maze(int);
 void wwcd(double);
 void loadscreen();
-int maze(int);
-void exit_game();
-void shuffle();
-int generate_maze(int,int);
-void bt(int,int,int);
-int search_destination(int,int);
-void settings();
 void screenfix();
+int generate_maze(int, int);
+void generate_ep(int);
+void bt(int, int, int);
+int search_destination(int, int);
+void shuffle();
+void gotoxy(int, int);
+void test();
